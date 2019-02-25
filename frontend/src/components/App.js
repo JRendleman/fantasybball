bb3fc978a6b309efe06153717980fa331d9df5ea
John Rendleman <johnrendleman@gmail.com>
2019-02-25 12:37:26 -0500

    Made first half of metrix maker

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header.js"
+import MetrixMaker from "./MetrixMaker.js"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MetrixMaker />
+        </div>
+    )
+}
+
+export default App
