cbe3247bf2f8efaeecec292d1d518a0e3b2ce691
John Rendleman <johnrendleman@gmail.com>
2019-03-24 18:53:33 -0400

    Finally got that player data rendered baby.

--- src/components/StatsTable.js
+++ src/components/StatsTable.js
@@ -1,85 +1,97 @@
 import React from "react"
 import ReactTable from "react-table";
+import Firebase from "./firebase"
 import "react-table/react-table.css"
 
+require("firebase");
 
-var names = ["James Harden", "Russel Westbrook", "Anthony Davis", "LeBron James", "Victor Oladipo", "Paul George", "Ben Simmons", "John Wall"]
+const firebase_client = new Firebase();
+
+let player_names = firebase_client.db.ref("players");
+
 var players = [];
-
-function addPlayerData() {
-    for (var i = 0; i < 8; i++) {
-        players.push({
-            player: names[i],
-            rating: Math.floor((Math.random() * 10) + 80),
-            rebounds: Math.floor((Math.random() * 11) + 1),
-            points: Math.floor((Math.random() * 15) + 20),
-            assists: Math.floor((Math.random() * 10) + 2),
-            steals: Math.floor((Math.random() * 3) + 0),
-            blocks: Math.floor((Math.random() * 2) + 1),
-            fieldgoal: .486,
-            threepoint: .446,
-            freethrow: 86.6,
-            turnovers: Math.floor((Math.random() * 4) + 0)
-            
-        })
-    };
-}
-
-addPlayerData();
 
 class StatsTable extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            data: []
+        }
+    }
+    
+    componentWillMount() {
+        player_names.on('value', (snapshot) => {
+            console.log(snapshot.val())
+            if(snapshot.exists()) {
+                console.log(snapshot)
+                snapshot.forEach((data) => {
+                var val = data.val();
+                players.push({
+                    name: val.name,
+                    ppg: val.ppg,
+                    reb: val.reb,
+                    ast: val.ast,
+                    stl: val.stl,
+                    blk: val.blk,
+                    fg: val.fg,
+                    tpt: val.tpt,
+                    ft: val.ft,
+                    to: val.to
+                });
+                    
+                
+        });
+                
+    }
+            this.setState({data: players});
+}); 
     }
 
     render() {
         const columns = [
             {
                 Header: "PLAYER",
-                accessor: "player"
-            },
-            {
-                Header: "RTG",
-                accessor: "rating"
+                accessor: "name"
             },
             {
                 Header: "PPG",
-                accessor: "points"
+                accessor: "ppg"
             },
             {
                 Header: "REB",
-                accessor: "rebounds"
+                accessor: "reb"
             },
             {
                 Header: "STL",
-                accessor: "steals"
+                accessor: "stl"
             },
             {
                 Header: "BLK",
-                accessor: "blocks"
+                accessor: "blk"
             },
             {
                 Header: "FG%",
-                accessor: "fieldgoal"
+                accessor: "fg"
             },
             {
                 Header: "3PT%",
-                accessor: "threepoint"
+                accessor: "tpt"
             },
             {
                 Header: "FT%",
-                accessor: "freethrow"
+                accessor: "ft"
             },
             {
                 Header: "TO",
-                accessor: "turnovers"
+                accessor: "to"
             }
         ]
         
         return (
                 <ReactTable id="react-table" 
                     columns={columns}
-                    data={players}/>
+                    data={this.state.data}/>
         )
     }
 }
