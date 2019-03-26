809b2e40b77cbec91a1432438e378ead3ea0a083
markp <markpetersen711@gmail.com>
2019-03-26 16:55:38 -0400

    deleted extra folder

--- fantasybball/src/components/CreateNewReport.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function CreateNewReport() {
-    return (
-        <input id="createNewReport" type="button" value="Create New Report" />
-    )
-}
-
-export default CreateNewReport
