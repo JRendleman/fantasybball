8d616520c4c9a0a72c8193bef28b86ea2f5df426
markp <markpetersen711@gmail.com>
2019-03-26 16:48:46 -0400

    connected filters with firebase

---
+++ fantasybball/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./Header.js"
+import MetrixMaker from "./MetrixMaker.js"
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MetrixMaker />
+        </div>
+    )
+}
+
+export default App
