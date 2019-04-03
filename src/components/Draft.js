a186181d186f087c77f5954d2e67a76c87a58def
John Rendleman <john.rendleman@gmail.com>
2019-04-02 20:26:32 -0400

    Added draft baord and player functionality

--- src/components/Draft.js
+++ src/components/Draft.js
@@ -1,6 +1,12 @@
 import React from "react";
 import DraftOverview from "./DraftOverview.js";
-import DraftPlayerDetail from "./DraftPlayerDetail.js"
+import DraftPlayerDetail from "./DraftPlayerDetail.js";
+import DraftBoard from "./DraftBoard.js";import Firebase from "./firebase"
+import "react-table/react-table.css"
+
+require('firebase');
+const firebase_client = new Firebase();
+let player_names = firebase_client.db.ref("players");
 
 // Function the pick number. 8 spots, 11 picks.
 function getLowestPickNumber(pick) {
@@ -11,45 +17,84 @@ function getLowestPickNumber(pick) {
     }
 }
 
-let player = {
-    ppg: 26.8,
-    reb: 8.6,
-    ast: 6.4,
-    stl: 1.8,
-    blk: 1.2,
-    salary: 2.4,
-    position: "SF",
-    name: "Kawhi Leonard",
-    team: "San Antonio Spurs"
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array
 }
 
 export default class Draft extends React.Component {
     constructor(props) {
         super(props);
 
-        this.players = [];
         this.teams = [];
-        this.draftOrder = [];
+        this.draftOrder = shuffleArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        this.draftPicks = [];
 
         this.state = {
+            selectedPlayer: null,
             round: 1,
             pick: 1,
             currentTeamId: 1,
-            isUser: false
+            isUser: false,
+            players: [],
         };  
     }
 
+    componentWillMount() {
+        player_names.on('value', (snapshot) => {
+            let players = [];
+            if(snapshot.exists()) {
+                snapshot.forEach((data) => {
+                var val = data.val();
+                players.push({
+                    name: val.name,
+                    ppg: val.ppg,
+                    reb: val.reb,
+                    ast: val.ast,
+                    stl: val.stl,
+                    blk: val.blk,
+                    fg: val.fg,
+                    tpt: val.tpt,
+                    ft: val.ft,
+                    to: val.to
+                });
+                    
+                
+        });        
+    }
+            console.log(players);
+            this.setState({players: players});
+        });
+    }
+
+    playerSelectedDraftBoard = (data) => {
+        this.setState({selectedPlayer: data});
+    }
+    
     render() {
+
         return(
             <div>
                 <DraftOverview 
                 lowestPick={getLowestPickNumber(this.state.pick)}
                 roundNumber = {this.state.round}
                 />
+
                 <DraftPlayerDetail 
-                player={player}
+                player={this.state.selectedPlayer}
                 draftNumber={this.state.pick}
                 />
+                <div id="draft-board">
+                <DraftBoard players={this.state.players} 
+                playerSelectedDraftBoard={this.playerSelectedDraftBoard}
+                />
+                </div>
             </div>
         )
     }
---
+++ src/components/DraftBoard.js
@@ -0,0 +1,101 @@
+import React from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css"
+
+export default class DraftBoard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.selectedPlayer = {}
+        this.state = {
+            players: props.players
+        }
+    }
+
+    playerSelected() {
+        this.props.playerSelectedDraftBoard(this.selectedPlayer);
+    }
+
+    render() {
+        const columns = [
+            {
+                Header: "PLAYER",
+                accessor: "name",
+                minWidth: 200
+            },
+            {
+                Header: "PPG",
+                accessor: "ppg",
+                width: 60
+            },
+            {
+                Header: "REB",
+                accessor: "reb",
+                width: 60
+            },
+            {
+                Header: "STL",
+                accessor: "stl",
+                width: 60
+            },
+            {
+                Header: "BLK",
+                accessor: "blk",
+                width: 60
+            },
+            {
+                Header: "FG%",
+                accessor: "fg",
+                width: 60
+            },
+            {
+                Header: "3PT%",
+                accessor: "tpt",
+                width: 60
+            },
+            {
+                Header: "FT%",
+                accessor: "ft",
+                width: 60
+            },
+            {
+                Header: "TO",
+                accessor: "to",
+                width: 60
+            }
+        ]
+        
+        return (
+                <ReactTable id="draft-board" 
+                    columns={columns}
+                    defaultSorted={[{
+                        id: "ppg",
+                        desc: true,
+                    }]}
+                    data={this.state.players}
+                    style={{
+                        width: '50%;'
+                    }}
+                    getTrProps={(state, rowInfo) => {
+                        if (rowInfo && rowInfo.row) {
+                          return {
+                            onClick: (e) => {
+                              this.selectedPlayer = rowInfo.original;
+                              this.playerSelected();
+                              this.setState({
+                                selected: rowInfo.index
+                              })
+                            },
+                            style: {
+                              background: rowInfo.index === this.state.selected ? '#00afec' : 'white',
+                              color: rowInfo.index === this.state.selected ? 'white' : 'black'
+                            }
+                          }
+                        }else{
+                          return {}
+                        }
+                      }
+                    }
+                    defaultPageSize={10}/>
+        )
+    }
+}
